ab86d61c821ed3da71315fb1aa7e42aa246b7d99
benjaminsalon <benjaminsalon.business@gmail.com>
2023-12-07 18:46:24 +0100

    test basic

--- contracts/chat/src/test.rs
+++ contracts/chat/src/test.rs
@@ -1,21 +1,24 @@
 #![cfg(test)]
 
-use super::*;
-use soroban_sdk::{Env, String};
-
+use crate::{ChatContract, ChatContractClient};
+use soroban_sdk::{testutils::Address as _, Address, Env, String};
 
 #[test]
 fn test() {
     let env = Env::default();
-    let contract_id = env.register_contract(None, TitleContract);
-    let client = TitleContractClient::new(&env, &contract_id);
 
-    let client_default_title = client.read_title(); 
-    assert_eq!(client_default_title, String::from_slice(&env, "Default Title"));
+    let from = Address::random(&env);
+    let to = Address::random(&env);
 
-    client.set_title(&String::from_slice(&env, "My New Title"));
-    let client_new_title = client.read_title(); 
+    let contract_id = env.register_contract(None, ChatContract);
+    let client = ChatContractClient::new(&env, &contract_id);
 
-    assert_eq!(client_new_title, String::from_slice(&env, "My New Title"));
+    client.write_message(&from, &to, &String::from_slice(&env, "Bonjour l'ami!"));
+    // let client_default_title = client.read_title();
+    // assert_eq!(client_default_title, String::from_slice(&env, "Default Title"));
 
+    // client.set_title(&String::from_slice(&env, "My New Title"));
+    // let client_new_title = client.read_title();
+
+    // assert_eq!(client_new_title, String::from_slice(&env, "My New Title"));
 }
