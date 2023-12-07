6b998caae1a268a8f5978da02007cbe2e849c195
benjaminsalon <benjaminsalon.business@gmail.com>
2023-12-07 18:28:28 +0100

    write_message function not tested

--- contracts/chat/src/lib.rs
+++ contracts/chat/src/lib.rs
@@ -12,11 +12,37 @@ pub struct Message {
 
 type Conversation = Vec<Message>;
 
+type ConversationsInitiated = Vec<Address>;
+
 #[derive(Clone)]
 #[contracttype]
 pub enum DataKey {
     Conversations(ConversationsKey),
-    ConversationInitiated(Address),
+    ConversationsInitiated(Address),
+}
+
+pub fn update_conversations_initiated(env: &Env, from: Address, to: Address) {
+    let mut conversations_initiated_from = env
+        .storage()
+        .instance()
+        .get::<_, ConversationsInitiated>(&DataKey::ConversationsInitiated(from.clone()))
+        .unwrap_or(vec![&env]);
+    conversations_initiated_from.push_back(to.clone());
+    env.storage().instance().set(
+        &DataKey::ConversationsInitiated(from.clone()),
+        &conversations_initiated_from,
+    );
+
+    let mut conversations_initiated_to = env
+        .storage()
+        .instance()
+        .get::<_, ConversationsInitiated>(&DataKey::ConversationsInitiated(to.clone()))
+        .unwrap_or(vec![&env]);
+    conversations_initiated_to.push_back(from.clone());
+    env.storage().instance().set(
+        &DataKey::ConversationsInitiated(to.clone()),
+        &conversations_initiated_to,
+    );
 }
 
 #[contract]
@@ -26,11 +52,27 @@ pub struct ChatContract;
 impl ChatContract {
     pub fn write_message(env: Env, from: Address, to: Address, msg: String) {
         // First we need to retrieve the possibly already existing conversation between from and to
-        let conversation = env
+        let key = DataKey::Conversations(ConversationsKey(from.clone(), to.clone()));
+
+        // We want to update the Conversation Initiated storage if it's the first time we have a conversation
+        let conversation_exists = env.storage().instance().has(&key);
+        if !conversation_exists {
+            update_conversations_initiated(&env, from.clone(), to.clone())
+        }
+
+        // Then we can retrieve the conversation
+        let mut conversation = env
             .storage()
             .instance()
-            .get::<_, Conversation>(&DataKey::Conversations(ConversationsKey(from, to)))
+            .get::<_, Conversation>(&key)
             .unwrap_or(vec![&env]);
+
+        // Then we can add a new message to the conversation
+        let new_message = Message { msg };
+        conversation.push_back(new_message);
+
+        // And we don't forget to set the state storage with the new value
+        env.storage().instance().set(&key.clone(), &conversation);
     }
 }
 
