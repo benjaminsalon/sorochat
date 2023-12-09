ac672145aa49e4e6641763ee6cdbb5b0bec94c6f
benjaminsalon <benjaminsalon.business@gmail.com>
2023-12-09 20:28:30 +0100

    Little color

--- src/components/chat/Message.tsx
+++ src/components/chat/Message.tsx
@@ -12,10 +12,11 @@ interface MessageProps {
 
 export function Message({message,userConnected}:MessageProps) {
     let position = userConnected == message.from ? "right" : "left";
+    let bg_color = userConnected == message.from ? "whiteAlpha.200" : "whiteAlpha.500";
     return (
         <>
-            <Box ml='3' bg="whiteAlpha.100" borderRadius="12" padding="4" margin="4" position="relative" >
-                <Text fontWeight='bold'>
+            <Box ml='3' bg={bg_color} borderRadius="12" padding="4" margin="4" position="relative" >
+                <Text fontWeight='bold' fontSize={12}>
                 {message.from}
                 </Text>
                 <Text fontSize='sm'>{message.msg}</Text>
