7f7cfa7af35afb185c830cb7ae98b5f55a0ab8b8
benjaminsalon <benjaminsalon.business@gmail.com>
2024-05-12 18:04:43 +0200

    Update of the chat contract

--- contracts/chat/src/lib.rs
+++ contracts/chat/src/lib.rs
@@ -1,9 +1,5 @@
 #![no_std]
-use soroban_sdk::{contract, contractimpl, contracttype, vec, Address, Env, String, Vec};
-
-#[derive(Clone)]
-#[contracttype]
-pub struct ConversationsKey(pub Address, pub Address);
+use soroban_sdk::{contract, contractimpl, Env, String, Address, Vec, contracttype, vec};
 
 #[contracttype]
 #[derive(Clone, Debug)]
@@ -18,34 +14,32 @@ type ConversationsInitiated = Vec<Address>;
 
 #[derive(Clone)]
 #[contracttype]
+pub struct ConversationsKey(pub Address, pub Address);
+
+#[derive(Clone)]
+#[contracttype]
 pub enum DataKey {
     Conversations(ConversationsKey),
     ConversationsInitiated(Address),
 }
 
 pub fn update_conversations_initiated(env: &Env, from: Address, to: Address) {
-    let mut conversations_initiated_from = env
-        .storage()
-        .persistent()
-        .get::<_, ConversationsInitiated>(&DataKey::ConversationsInitiated(from.clone()))
-        .unwrap_or(vec![&env]);
-    conversations_initiated_from.push_back(to.clone());
+    // Update the conversation initiated of FROM
+    let mut conversation_initiated_from = env.storage().persistent().get::<_, ConversationsInitiated>(&DataKey::ConversationsInitiated(from.clone())).unwrap_or(vec![&env]);
+    conversation_initiated_from.push_back(to.clone());
     env.storage().persistent().set(
         &DataKey::ConversationsInitiated(from.clone()),
-        &conversations_initiated_from,
+        &conversation_initiated_from
     );
 
-    // If we are sending chat to ourselves, we don't want to have two different conversations
+
+    // Update the conversation initiated of TO
     if from != to {
-        let mut conversations_initiated_to = env
-            .storage()
-            .persistent()
-            .get::<_, ConversationsInitiated>(&DataKey::ConversationsInitiated(to.clone()))
-            .unwrap_or(vec![&env]);
-        conversations_initiated_to.push_back(from.clone());
+        let mut conversation_initiated_to = env.storage().persistent().get::<_, ConversationsInitiated>(&DataKey::ConversationsInitiated(to.clone())).unwrap_or(vec![&env]);
+        conversation_initiated_to.push_back(from.clone());
         env.storage().persistent().set(
             &DataKey::ConversationsInitiated(to.clone()),
-            &conversations_initiated_to,
+            &conversation_initiated_to
         );
     }
 }
@@ -55,67 +49,57 @@ pub struct ChatContract;
 
 #[contractimpl]
 impl ChatContract {
-    pub fn write_message(env: Env, from: Address, to: Address, msg: String) {
-        // Ensure that the message sender is the from address
-        from.require_auth();
 
-        // First we need to retrieve the possibly already existing conversation between from and to
-        let key = DataKey::Conversations(ConversationsKey(from.clone(), to.clone()));
-
-        // We want to update the Conversation Initiated storage if it's the first time we have a conversation between from and to
-        let conversation_exists =
-            env.storage().persistent().has(&key) && env.storage().persistent().has(&key);
-        if !conversation_exists {
-            update_conversations_initiated(&env, from.clone(), to.clone())
-        }
-
-        // Then we can retrieve the conversation
-        let mut conversation = env
-            .storage()
-            .persistent()
-            .get::<_, Conversation>(&key)
-            .unwrap_or(vec![&env]);
-
-        // Then we can add a new message to the conversation
-        let new_message = Message {
-            msg,
-            from: from.clone(),
-        };
-        conversation.push_back(new_message);
-
-        // And we don't forget to set the state storage with the new value ON BOTH SIDES if not conversation to self
-        env.storage().persistent().set(&key.clone(), &conversation);
-        if from != to {
-            let key_other_side = DataKey::Conversations(ConversationsKey(to.clone(), from.clone()));
-            env.storage()
-                .persistent()
-                .set(&key_other_side.clone(), &conversation);
-        }
+    pub fn read_title(env: Env) -> String {
+        String::from_str(&env, "This is a success")
     }
 
-    pub fn read_conversation(env: Env, from: Address, to: Address) -> Conversation {
-        let key = DataKey::Conversations(ConversationsKey(from.clone(), to.clone()));
-        let conversation = env
-            .storage()
-            .persistent()
-            .get::<_, Conversation>(&key)
-            .unwrap_or(vec![&env]);
+    pub fn read_conversation(env: &Env, from: Address, to:Address) -> Conversation {
+        let key = DataKey::Conversations(ConversationsKey(from.clone(),to.clone()));
+        let conversation = env.storage().persistent().get::<_,Conversation>(&key).unwrap_or(vec![&env]);
         conversation
     }
 
-    pub fn read_conversations_initiated(env: Env, from: Address) -> ConversationsInitiated {
+    pub fn read_conversations_initiated(env: &Env, from: Address) -> ConversationsInitiated{
         let key = DataKey::ConversationsInitiated(from);
-        let conversations_initiated = env
-            .storage()
-            .persistent()
-            .get::<_, ConversationsInitiated>(&key)
-            .unwrap_or(vec![&env]);
+        let conversations_initiated = env.storage().persistent().get::<_,ConversationsInitiated>(&key).unwrap_or(vec![&env]);
         conversations_initiated
     }
 
-    pub fn read_title(env: Env) -> String {
-        String::from_slice(&env, "Welcome to Sorochat!")
+    pub fn write_message(env: Env, from: Address, to: Address, msg_str: String) {
+        // Check that from is authorized
+        from.require_auth();
+
+        // Retrieve the conversation key
+        let key = DataKey::Conversations(ConversationsKey(from.clone(), to.clone()));
+
+        // Check if conversation exists
+        let conversation_exists = env.storage().persistent().has(&key);
+
+        // If it does not exists, then we add the addresses of from and to to respectively to and from initiated conversations
+        if !conversation_exists {
+            update_conversations_initiated(&env, from.clone(), to.clone());
+        }
+
+        // We retrieve the conversation, if conversation does not exist -> empty vec
+        let mut conversation = env.storage().persistent().get::<_,Conversation>(&key).unwrap_or(vec![&env]);
+
+        // Add the new message to the conversation
+        let new_message = Message {
+            msg: msg_str,
+            from: from.clone()
+        };
+        conversation.push_back(new_message);
+
+        // Push the updated conversation back into the storage
+        env.storage().persistent().set(&key, &conversation);
+        if from != to {
+            let key_other_side = DataKey::Conversations(ConversationsKey(to.clone(), from.clone()));
+            env.storage().persistent().set(&key_other_side, &conversation);
+        }
     }
+
+
 }
 
 mod test;
